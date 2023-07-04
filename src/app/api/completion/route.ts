0a69916dbdb90769c2e271661cecc204156de88a
Yi Hein Chai <chaiyihein@gmail.com>
2023-07-04 20:06:51 +0100

    retry cors resolving

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -11,14 +11,11 @@ export async function POST(req: NextRequest) {
   const stream = await call_gpt_with_stream(reqBody.prompt, reqBody?.chat_history);
 
   return new Response(stream, {
-    headers: new Headers({
-      // since we don't use browser's EventSource interface, specifying content-type is optional.
-      // the eventsource-parser library can handle the stream response as SSE, as long as the data format complies with SSE:
-      // https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#sending_events_from_the_server
-
-      // 'Content-Type': 'text/event-stream',
-      "Cache-Control": "no-cache",
+    headers: {
       "Access-Control-Allow-Origin": "*",
-    }),
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Cache-Control": "no-cache",
+    },
   });
 }
