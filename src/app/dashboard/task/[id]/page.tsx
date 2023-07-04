b5c24f34491c7898a13c00bc549ad9a16ee7a3da
Yi Hein Chai <chaiyihein@gmail.com>
2023-07-04 18:53:09 +0100

    remove wrapper margin of task details

--- src/app/dashboard/task/[id]/page.tsx
+++ src/app/dashboard/task/[id]/page.tsx
@@ -16,51 +16,47 @@ export async function generateStaticParams() {
 export default async function Page({ params }: { params: { id: string } }) {
   const task = await getTask(params.id);
   return (
-    <div className="m-3">
-      <div className="w-full px-2 bg-white dark:bg-gray-800">
-        <div className="z-100 sticky top-0 bg-white dark:bg-gray-800 px-5 pt-8 pb-1">
-          <div className="flex items-center justify-between">
-            <span className="text-sm font-light text-gray-800 dark:text-gray-400">
-              {task.assignee}
-            </span>
-            <Chip name={task.status || "TODO"} color={STATUS_COLORS[task.status || "TODO"]} />
-          </div>
-
-          <div>
-            <h1 className="mt-2 text-xl font-semibold text-gray-800 dark:text-white">
-              {task.name}
-            </h1>
-            <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">{task.description}</p>
-          </div>
+    <div className="w-full px-2 bg-white dark:bg-gray-800">
+      <div className="z-100 sticky top-0 bg-white dark:bg-gray-800 px-5 pt-8 pb-1">
+        <div className="flex items-center justify-between">
+          <span className="text-sm font-light text-gray-800 dark:text-gray-400">
+            {task.assignee}
+          </span>
+          <Chip name={task.status || "TODO"} color={STATUS_COLORS[task.status || "TODO"]} />
         </div>
 
+        <div>
+          <h1 className="mt-2 text-xl font-semibold text-gray-800 dark:text-white">{task.name}</h1>
+          <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">{task.description}</p>
+        </div>
+      </div>
+
+      <div className="px-5 py-4">
+        <div className="p-4 bg-gray-100 rounded-lg dark:bg-gray-700">
+          <h2 className="text-base font-medium text-gray-800 dark:text-white">Actions</h2>
+          <div className="mt-2 text-sm text-gray-500 dark:text-gray-400">
+            <TaskActionsInline task={task} />
+          </div>
+        </div>
+      </div>
+
+      {task.subtasks.length > 0 && (
         <div className="px-5 py-4">
-          <div className="p-4 bg-gray-100 rounded-lg dark:bg-gray-700">
-            <h2 className="text-base font-medium text-gray-800 dark:text-white">Actions</h2>
-            <div className="mt-2 text-sm text-gray-500 dark:text-gray-400">
-              <TaskActionsInline task={task} />
-            </div>
-          </div>
+          <TaskList withHeader tasks={task.subtasks} />{" "}
         </div>
+      )}
 
-        {task.subtasks.length > 0 && (
-          <div className="px-5 py-4">
-            <TaskList withHeader tasks={task.subtasks} />{" "}
-          </div>
-        )}
-
-        <div className="px-5 py-4">
-          <div className="p-4 bg-gray-100 rounded-lg dark:bg-gray-700">
-            <h2 className="text-base font-medium text-gray-800 dark:text-white">
-              Final Execution Result
-            </h2>
-            <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
-              {task.execution_result || "Not executed yet"}
-            </p>
-          </div>
-          {/* Content */}
-          <Chat task={task} />
+      <div className="px-5 py-4">
+        <div className="p-4 bg-gray-100 rounded-lg dark:bg-gray-700">
+          <h2 className="text-base font-medium text-gray-800 dark:text-white">
+            Final Execution Result
+          </h2>
+          <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
+            {task.execution_result || "Not executed yet"}
+          </p>
         </div>
+        {/* Content */}
+        <Chat task={task} />
       </div>
     </div>
   );
